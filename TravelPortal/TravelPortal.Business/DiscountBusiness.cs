6b64c39134f57aa28632df1db009267eb83b234d
Saurabh Vijayvargiya <saurabh.vijayvargiya@nagarro.com>
2024-03-04 13:17:19 +0530

    Adding Discount controller

---
+++ TravelPortal/TravelPortal.Business/DiscountBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPortal.Business
+{
+    public class DiscountBusiness
+    {
+        public long GetDiscountForItem(int itemPrice, int quantity, int discountPercent)
+        {
+            return (itemPrice * quantity * discountPercent) / 100 + (itemPrice * quantity)/discountPercent;
+        }
+    }
+}
---
+++ TravelPortal/TravelPortal/Controllers/DiscountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelPortal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscountController
+    {
+        [HttpGet(Name = "GetDiscount")]
+        public long GetDiscountForItem(int itemPrice, int quantity, int discountPercent)
+        {
+            // Call the business layer to get the discount
+            return (itemPrice * quantity * discountPercent) / 100 + (itemPrice * quantity) / discountPercent;
+        }
+    }
+}
